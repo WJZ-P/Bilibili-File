2616a748fc813a317c85f947862d4797d8b47530
WJZ_P <110795301+WJZ-P@users.noreply.github.com>
2025-06-01 14:28:46 +0800

    样式修改

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,7 @@ import MainMenu from "./components/MainMenu.vue";
 <style>
 .main-container {
   width: 100%;
-  height: 100%;
+  height: 95%;
   display: flex;
   justify-content: center;
   align-items: center;
--- src/components/MainMenu.vue
+++ src/components/MainMenu.vue
@@ -64,100 +64,106 @@
 
       <!-- 文件列表 -->
       <div class="file-list">
-        <!-- 表头 -->
-        <div class="list-header">
-          <div class="col-name">文件名</div>
-          <div class="col-size" style="text-align: center">大小</div>
-          <div class="col-date" style="text-align: center">上传日期</div>
-          <div class="col-actions" style="text-align: center">操作</div>
-        </div>
+        <table class="file-table">
+          <!-- 表头 -->
+          <thead>
+            <tr>
+              <th class="col-name">文件名</th>
+              <th class="col-size">大小</th>
+              <th class="col-date">上传日期</th>
+              <th class="col-actions">操作</th>
+            </tr>
+          </thead>
 
-        <!-- 动态文件项 -->
-        <div
-            class="list-item"
-            v-for="file in filteredFiles"
-            :key="file.name + file.size"
-        >
-          <div class="col-name" style="text-align: center" @click="copyFileLink(file,$event)" :title="'点击复制下载链接'">
-            <i v-if="isImage(file)" class="iconfont icon-file">
-              <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"
-                   fill="#5f6368">
-                <path
-                    d="M212.31-140Q182-140 161-161q-21-21-21-51.31v-535.38Q140-778 161-799q21-21 51.31-21h535.38Q778-820 799-799q21 21 21 51.31v535.38Q820-182 799-161q-21 21-51.31 21H212.31Zm0-60h535.38q4.62 0 8.46-3.85 3.85-3.84 3.85-8.46v-535.38q0-4.62-3.85-8.46-3.84-3.85-8.46-3.85H212.31q-4.62 0-8.46 3.85-3.85 3.84-3.85 8.46v535.38q0 4.62 3.85 8.46 3.84 3.85 8.46 3.85ZM270-290h423.07L561.54-465.38 449.23-319.23l-80-102.31L270-290Zm-70 90v-560 560Z"/>
-              </svg>
-            </i>
-            <i v-else-if="isCompressed(file)" class="iconfont icon-file">
-              <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"
-                   fill="#5f6368">
-                <path
-                    d="M640-480v-80h80v80h-80Zm0 80h-80v-80h80v80Zm0 80v-80h80v80h-80ZM447.38-640l-80-80H172.31q-5.39 0-8.85 3.46t-3.46 8.85v455.38q0 5.39 3.46 8.85t8.85 3.46H560v-80h80v80h147.69q5.39 0 8.85-3.46t3.46-8.85v-375.38q0-5.39-3.46-8.85t-8.85-3.46H640v80h-80v-80H447.38ZM172.31-180Q142-180 121-201q-21-21-21-51.31v-455.38Q100-738 121-759q21-21 51.31-21h219.61l80 80h315.77Q818-700 839-679q21 21 21 51.31v375.38Q860-222 839-201q-21 21-51.31 21H172.31ZM160-240v-480 480Z"/>
-              </svg>
-            </i>
-            <i v-else class="iconfont icon-file">
-              <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"
-                   fill="#5f6368">
-                <path
-                    d="M330-250h300v-60H330v60Zm0-160h300v-60H330v60Zm-77.69 310Q222-100 201-121q-21-21-21-51.31v-615.38Q180-818 201-839q21-21 51.31-21H570l210 210v477.69Q780-142 759-121q-21 21-51.31 21H252.31ZM540-620v-180H252.31q-4.62 0-8.46 3.85-3.85 3.84-3.85 8.46v615.38q0 4.62 3.85 8.46 3.84 3.85 8.46 3.85h455.38q4.62 0 8.46-3.85 3.85-3.84 3.85-8.46V-620H540ZM240-800v180-180V-160v-640Z"/>
-              </svg>
-            </i>
-            {{ file.name }}
-          </div>
-          <div class="col-size" style="text-align: center">{{ formatSize(file.size) }}</div>
-          <div class="col-date" style="text-align: center">{{ formatDate(file.lastModified) }}</div>
-          <div class="col-actions">
-            <button
-                v-if="isImage(file)"
-                class="btn-preview"
-                @click.stop="handlePreview(file)"
+          <!-- 动态文件项 -->
+          <tbody>
+            <tr
+                v-for="file in filteredFiles"
+                :key="file.name + file.size"
+                class="file-row"
             >
-              <i class="iconfont icon-preview">
-                <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px"
-                     fill="#5f6368">
-                  <path
-                      d="M480.09-336.92q67.99 0 115.49-47.59t47.5-115.58q0-67.99-47.59-115.49t-115.58-47.5q-67.99 0-115.49 47.59t-47.5 115.58q0 67.99 47.59 115.49t115.58 47.5ZM480-392q-45 0-76.5-31.5T372-500q0-45 31.5-76.5T480-608q45 0 76.5 31.5T588-500q0 45-31.5 76.5T480-392Zm.05 172q-137.97 0-251.43-76.12Q115.16-372.23 61.54-500q53.62-127.77 167.02-203.88Q341.97-780 479.95-780q137.97 0 251.43 76.12Q844.84-627.77 898.46-500q-53.62 127.77-167.02 203.88Q618.03-220 480.05-220ZM480-500Zm0 220q113 0 207.5-59.5T832-500q-50-101-144.5-160.5T480-720q-113 0-207.5 59.5T128-500q50 101 144.5 160.5T480-280Z"/>
-                </svg>
-              </i>
-              <span>预览</span>
-            </button>
-            <button
-                v-if="file.status === 'success'"
-                class="btn-download"
-                @click.stop="handleDownload(file)"
-            >
-              <i class="iconfont icon-download">
-                <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px"
-                     fill="#5f6368">
-                  <path
-                      d="M480-328.46 309.23-499.23l42.16-43.38L450-444v-336h60v336l98.61-98.61 42.16 43.38L480-328.46ZM252.31-180Q222-180 201-201q-21-21-21-51.31v-108.46h60v108.46q0 4.62 3.85 8.46 3.84 3.85 8.46 3.85h455.38q4.62 0 8.46-3.85 3.85-3.84 3.85-8.46v-108.46h60v108.46Q780-222 759-201q-21 21-51.31 21H252.31Z"/>
-                </svg>
-              </i>
-              <span>下载</span>
-            </button>
-            <button
-                class="btn-delete"
-                @click.stop="handleDelete(file)"
-            >
-              <i class="iconfont icon-delete">
-                <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px"
-                     fill="#5f6368">
-                  <path
-                      d="M292.31-140q-29.92 0-51.12-21.19Q220-182.39 220-212.31V-720h-40v-60h180v-35.38h240V-780h180v60h-40v507.69Q740-182 719-161q-21 21-51.31 21H292.31ZM680-720H280v507.69q0 5.39 3.46 8.85t8.85 3.46h375.38q4.62 0 8.46-3.85 3.85-3.84 3.85-8.46V-720ZM376.16-280h59.99v-360h-59.99v360Zm147.69 0h59.99v-360h-59.99v360ZM280-720v520-520Z"/>
-                </svg>
-              </i>
-              <span>删除</span>
-            </button>
-          </div>
-          <!-- 添加进度条 -->
-          <div class="progress-bar" v-if="fileProgress[file.name]">
-            <div class="progress-inner" :style="{ width: `${fileProgress[file.name]}%` }"></div>
-          </div>
-        </div>
+              <td class="col-name" @click="copyFileLink(file,$event)" :title="'点击复制下载链接'">
+                <div class="file-name-cell">
+                  <i v-if="isImage(file)" class="iconfont icon-file">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"
+                         fill="#5f6368">
+                      <path
+                          d="M212.31-140Q182-140 161-161q-21-21-21-51.31v-535.38Q140-778 161-799q21-21 51.31-21h535.38Q778-820 799-799q21 21 21 51.31v535.38Q820-182 799-161q-21 21-51.31 21H212.31Zm0-60h535.38q4.62 0 8.46-3.85 3.85-3.84 3.85-8.46v-535.38q0-4.62-3.85-8.46-3.84-3.85-8.46-3.85H212.31q-4.62 0-8.46 3.85-3.85 3.84-3.85 8.46v535.38q0 4.62 3.85 8.46 3.84 3.85 8.46 3.85ZM270-290h423.07L561.54-465.38 449.23-319.23l-80-102.31L270-290Zm-70 90v-560 560Z"/>
+                    </svg>
+                  </i>
+                  <i v-else-if="isCompressed(file)" class="iconfont icon-file">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"
+                         fill="#5f6368">
+                      <path
+                          d="M640-480v-80h80v80h-80Zm0 80h-80v-80h80v80Zm0 80v-80h80v80h-80ZM447.38-640l-80-80H172.31q-5.39 0-8.85 3.46t-3.46 8.85v455.38q0 5.39 3.46 8.85t8.85 3.46H560v-80h80v80h147.69q5.39 0 8.85-3.46t3.46-8.85v-375.38q0-5.39-3.46-8.85t-8.85-3.46H640v80h-80v-80H447.38ZM172.31-180Q142-180 121-201q-21-21-21-51.31v-455.38Q100-738 121-759q21-21 51.31-21h219.61l80 80h315.77Q818-700 839-679q21 21 21 51.31v375.38Q860-222 839-201q-21 21-51.31 21H172.31ZM160-240v-480 480Z"/>
+                    </svg>
+                  </i>
+                  <i v-else class="iconfont icon-file">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"
+                         fill="#5f6368">
+                      <path
+                          d="M330-250h300v-60H330v60Zm0-160h300v-60H330v60Zm-77.69 310Q222-100 201-121q-21-21-21-51.31v-615.38Q180-818 201-839q21-21 51.31-21H570l210 210v477.69Q780-142 759-121q-21 21-51.31 21H252.31ZM540-620v-180H252.31q-4.62 0-8.46 3.85-3.85 3.84-3.85 8.46v615.38q0 4.62 3.85 8.46 3.84 3.85 8.46 3.85h455.38q4.62 0 8.46-3.85 3.85-3.84 3.85-8.46V-620H540ZM240-800v180-180V-160v-640Z"/>
+                    </svg>
+                  </i>
+                  <span class="file-name">{{ file.name }}</span>
+                </div>
+              </td>
+              <td class="col-size">{{ formatSize(file.size) }}</td>
+              <td class="col-date">{{ formatDate(file.lastModified) }}</td>
+              <td class="col-actions">
+                <div class="action-buttons">
+                  <button
+                      v-if="isImage(file)"
+                      class="btn-preview"
+                      @click.stop="handlePreview(file)"
+                  >
+                    <i class="iconfont icon-preview">
+                      <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px"
+                           fill="#5f6368">
+                        <path
+                            d="M480.09-336.92q67.99 0 115.49-47.59t47.5-115.58q0-67.99-47.59-115.49t-115.58-47.5q-67.99 0-115.49 47.59t-47.5 115.58q0 67.99 47.59 115.49t115.58 47.5ZM480-392q-45 0-76.5-31.5T372-500q0-45 31.5-76.5T480-608q45 0 76.5 31.5T588-500q0 45-31.5 76.5T480-392Zm.05 172q-137.97 0-251.43-76.12Q115.16-372.23 61.54-500q53.62-127.77 167.02-203.88Q341.97-780 479.95-780q137.97 0 251.43 76.12Q844.84-627.77 898.46-500q-53.62 127.77-167.02 203.88Q618.03-220 480.05-220ZM480-500Zm0 220q113 0 207.5-59.5T832-500q-50-101-144.5-160.5T480-720q-113 0-207.5 59.5T128-500q50 101 144.5 160.5T480-280Z"/>
+                      </svg>
+                    </i>
+                    <span>预览</span>
+                  </button>
+                  <button
+                      v-if="file.status === 'success'"
+                      class="btn-download"
+                      @click.stop="handleDownload(file)"
+                  >
+                    <i class="iconfont icon-download">
+                      <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px"
+                           fill="#5f6368">
+                        <path
+                            d="M480-328.46 309.23-499.23l42.16-43.38L450-444v-336h60v336l98.61-98.61 42.16 43.38L480-328.46ZM252.31-180Q222-180 201-201q-21-21-21-51.31v-108.46h60v108.46q0 4.62 3.85 8.46 3.84 3.85 8.46 3.85h455.38q4.62 0 8.46-3.85 3.85-3.84 3.85-8.46v-108.46h60v108.46Q780-222 759-201q-21 21-51.31 21H252.31Z"/>
+                      </svg>
+                    </i>
+                    <span>下载</span>
+                  </button>
+                  <button
+                      class="btn-delete"
+                      @click.stop="handleDelete(file)"
+                  >
+                    <i class="iconfont icon-delete">
+                      <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px"
+                           fill="#5f6368">
+                        <path
+                            d="M292.31-140q-29.92 0-51.12-21.19Q220-182.39 220-212.31V-720h-40v-60h180v-35.38h240V-780h180v60h-40v507.69Q740-182 719-161q-21 21-51.31 21H292.31ZM680-720H280v507.69q0 5.39 3.46 8.85t8.85 3.46h375.38q4.62 0 8.46-3.85 3.85-3.84 3.85-8.46V-720ZM376.16-280h59.99v-360h-59.99v360Zm147.69 0h59.99v-360h-59.99v360ZM280-720v520-520Z"/>
+                      </svg>
+                    </i>
+                    <span>删除</span>
+                  </button>
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
 
 
     </div>
     <div class="footer-info">
-      © 2024 <a href="https://github.com/WJZ-P/Bilibili-File" target="_blank" title="GitHub地址">Bilibili File</a>.
+      © 2025 <a href="https://github.com/WJZ-P/Bilibili-File" target="_blank" title="GitHub地址">Bilibili File</a>.
       Made by WJZ_P with love. （￣▽￣）
     </div>
   </div>
@@ -483,7 +489,7 @@ const copyFileLink = async (file, event) => {
   }
 };
 
-// 修改分享链接下载函数
+// 分享链接下载函数
 const handleShareLinkDownload = async () => {
   if (!shareLink.value.trim()) {
     showToastMessage('请输入分享链接', { clientX: window.innerWidth / 2, clientY: 100 });
@@ -513,10 +519,7 @@ const handleShareLinkDownload = async () => {
     
     while(true) {
       const {done, value} = await reader.read();
-      
-      if (done) {
-        break;
-      }
+      if (done) break;
       
       chunks.push(value);
       receivedLength += value.length;
@@ -622,10 +625,11 @@ const handleShareLinkDownload = async () => {
 .main-menu {
   display: flex;
   flex-direction: column;
-  justify-content: start;
+  justify-content: flex-start;
   align-items: start;
   width: 100%;
   height: 100%;
+  overflow-x: hidden; /* 防止水平滚动 */
 }
 
 .file-manager {
@@ -640,6 +644,7 @@ const handleShareLinkDownload = async () => {
   height: 100%;
   justify-content: center;
   align-items: center;
+  box-sizing: border-box; /* 确保padding不会增加总宽度 */
 }
 
 /* 操作栏 */
@@ -792,106 +797,133 @@ const handleShareLinkDownload = async () => {
 /* 文件列表 */
 .file-list {
   width: 100%;
+  overflow-x: auto; /* 如果表格内容过宽，允许在列表区域内滚动 */
 }
 
-.list-header {
+.file-table {
   width: 100%;
-  display: grid;
-  grid-template-columns: 3fr 1fr 1.5fr 1fr;
-  padding: 12px 0;
+  border-collapse: collapse;
+  border-spacing: 0;
+  table-layout: fixed;
+  min-width: 800px; /* 设置最小宽度，防止表格过窄 */
+}
+
+.file-table th,
+.file-table td {
+  padding: 12px;
+  text-align: center;
   border-bottom: 1px solid #eee;
+}
+
+/* 设置各列的宽度 */
+.file-table .col-name {
+  width: 45%; 
+}
+
+.file-table .col-size {
+  width: 15%; 
+}
+
+.file-table .col-date {
+  width: 20%; 
+}
+
+.file-table .col-actions {
+  width: 20%; 
+}
+
+.file-table th {
   color: #666;
   font-weight: 500;
+  background: #fafafa;
 }
 
-.list-item {
-  width: 100%;
-  display: grid;
-  grid-template-columns: 3fr 1fr 1.5fr 1fr;
-  align-items: center;
-  padding: 15px 0;
-  border-bottom: 1px solid #f5f5f5;
+.file-row {
   transition: background 0.2s;
-  position: relative;
+}
+
+.file-row:hover {
+  background: #f9f9f9;
+}
+
+.file-name-cell {
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  cursor: pointer;
+  transition: color 0.2s;
+  text-align: left; 
+}
+
+.file-name-cell:hover {
+  color: #00aeec;
+}
+
+.file-name {
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+}
+
+.action-buttons {
+  display: flex;
+  gap: 8px;
+  justify-content: center;
+}
+
+.col-actions button {
+  background: #00c1ff;
+  border: none;
+  padding: 6px;
+  border-radius: 6px;
+  cursor: pointer;
+  transition: 0.2s;
+  width: 70px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 4px;
+
+  .iconfont {
+    color: white;
+    font-size: 18px;
+  }
 
   &:hover {
-    background: #f9f9f9;
+    background: #66ccff;
+    transform: translateY(-2px);
+    box-shadow: 0 3px 12px rgba(0, 174, 236, 0.3);
   }
 
-  .col-name {
-    display: flex;
-    align-items: center;
-    gap: 10px;
-    color: #333;
-    cursor: pointer;
-    transition: color 0.2s;
-
-    .iconfont {
-      font-size: 20px;
-      color: #00aeec;
-    }
-  }
-
-  .col-name:hover {
-    color: #00aeec;
-  }
-}
-
-.col-actions {
-  display: flex;
-  gap: 15px;
-  justify-content: center;
-
-  button {
-    background: #00c1ff;
-    border: none;
-    padding: 6px;
-    border-radius: 6px;
-    cursor: pointer;
-    transition: 0.2s;
-    width: 70px;
-    display: flex;
-    justify-content: center;
-
-    .iconfont {
-      color: white;
-      font-size: 18px;
-    }
+  &.btn-delete {
+    background: rgba(245, 75, 76, 0.86);
 
     &:hover {
-      background: #66ccff;
-      transform: translateY(-2px);
-      box-shadow: 0 3px 12px rgba(0, 174, 236, 0.3);
+      background: #ff6668;
+      box-shadow: 0 3px 12px rgba(255, 77, 79, 0.3);
     }
+  }
 
-    &.btn-delete {
-      background: rgba(245, 75, 76, 0.86);
-
-      &:hover {
-        background: #ff6668;
-        box-shadow: 0 3px 12px rgba(255, 77, 79, 0.3);
-      }
-    }
-
-    &.btn-preview {
-      background: rgba(0, 255, 67, 0.56);
-    }
+  &.btn-preview {
+    background: rgba(0, 255, 67, 0.56);
   }
 }
 
-
-/* 存储进度条 */
-.storage-progress {
-  height: 8px;
-  background: #f0f0f0;
-  border-radius: 4px;
+/* 进度条样式 */
+.progress-bar {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 2px;
+  background: rgba(0, 174, 236, 0.1);
   overflow: hidden;
+}
 
-  .progress-inner {
-    height: 100%;
-    background: #00aeec;
-    transition: width 0.3s;
-  }
+.progress-inner {
+  height: 100%;
+  background: #00aeec;
+  transition: width 0.3s ease;
 }
 
 /* 图标字体 */
@@ -1096,5 +1128,16 @@ const handleShareLinkDownload = async () => {
   height: 100%;
   background: #00aeec;
   transition: width 0.3s ease;
+}
+
+/* 修改操作按钮样式 */
+.action-table {
+  width: 100%;
+  border-collapse: collapse;
+}
+
+.action-table td {
+  padding: 0 5px;
+  text-align: center;
 }
 </style>
